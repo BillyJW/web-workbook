e711942dd1308bee5b10904c7244b7ef0a8e5ba0
Billy Watson <shadowsubz@gmail.com>
2018-08-27 11:46:42 -0500

    basic setup; one rule down

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
+// window.onload = function(){
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+
+var count = document.querySelectorAll('li')
+  console.log(count.length);
+
+  document.querySelector('h1').insertAdjacentHTML('afterEnd', '<h2> You have ' +
+ count.length + ' items in your shopping cart. </h2>');
+
+  });
+// }
+
+var newh2 = document.createElement('h2');
+newh2.innerHTML = `you have ${counts[0].children.length}
+items in your shopping cart`;
+docment.getElementByTagName('h1')[0].appendChild(newh2);
