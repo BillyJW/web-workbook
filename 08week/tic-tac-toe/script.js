04445e5df429be82cedefec2d119b01cdd7a8f57
Billy Watson <shadowsubz@gmail.com>
2018-09-05 19:21:48 -0500

    clearing tree

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,66 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic in here
-});
+$(function() {
+  var turn = 'X'
+  $('[data-cell]').on('click', function(){
+    if($(this).text() === ''){
+      $(this).text(turn);
+      checkWin();
+      if(turn === 'X'){
+        turn = 'O';
+      }
+    }else{
+      turn = 'X';
+    }
+  })
+  function checkWin(){
+    if($('[data-cell]="0"').text() === turn &&
+    $('d[ata-cell]="1"').text() === turn &&
+    $('[data-cell]="2"').text() === turn{
+      &('#announce-winner').text(`${turn} wins`);
+    }
+  }
+
+  $('#clear').click(function(){
+    $('{data-cell}').empty();
+    turn = "X";
+    $('#announce-winner').empty();
+  })
+
+})
+
+
+
+
+
+
+
+// $(function() {
+//   var turn = 'X'
+//   $(function() {
+//   $('[data-cell]').on('click', function(){
+//     if($(this).text() === ''){
+//       $(this).text(turn);
+//       if(turn === 'X'){
+//         turn = 'O';
+//       }
+//       // $(this).text('X');
+//     }else{
+//       turn = 'X';
+//     }
+//     limit(24);
+//   });
+//
+//   function limit(target){
+//     let total = 0;
+//     $('[data-cell]').each(function(){
+//      total += Number($(this).text());
+//       console.log(total);
+//     })
+//     if(total === target){
+//       $('#announce-winner').text(`HIT ${target}`);
+//     }
+//   }
+// })
+//
+// });
